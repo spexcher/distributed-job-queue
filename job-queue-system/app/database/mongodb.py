fc50a3241d4be85b96d0d2d911adbaaf50e1f1c1
Gourab Modak <137685981+spexcher@users.noreply.github.com>
2026-02-05 21:39:34 +0530

    Delete job-queue-system directory

--- job-queue-system/app/database/mongodb.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from app.config import settings
-
-client = MongoClient(settings.MONGO_URI)
-db = client[settings.DB_NAME]
-worker_collection = db["workers"]
-job_collection = db["jobs"]
